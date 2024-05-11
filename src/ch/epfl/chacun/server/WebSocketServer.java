1e7af6969ec0186ff6af5f57087bb5796961050d
Max <maxence1604.minecraft@gmail.com>
2024-05-11 15:54:39 +0200

    Use RegExp to find Sec-WebSocket-Key value refactor http handshake upgrade response and add GUID doc

--- src/ch/epfl/chacun/server/WebSocketServer.java
+++ src/ch/epfl/chacun/server/WebSocketServer.java
@@ -11,13 +11,40 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Iterator;
+import java.util.StringJoiner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebSocketServer {
 
-    private static final String MAGIC_NUMBER = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    /**
+     * The port number on which the server listens.
+     */
+    public static final int PORT = 3000;
 
+    /**
+     * Globally Unique Identifier (GUID, [RFC4122]) in string form.
+     */
+    private static final String GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+    /**
+     * Create the key for the "Sec-WebSocket-Accept" header of the handshake upgrade response.
+     * <p>
+     * For this header field, the server has to take the value (as present
+     * in the header field, e.g., the base64-encoded [RFC4648] version minus
+     * any leading and trailing whitespace) and concatenate this with the
+     * Globally Unique Identifier (GUID, [RFC4122]) "258EAFA5-E914-47DA-
+     * 95CA-C5AB0DC85B11" in string form, which is unlikely to be used by
+     * network endpoints that do not understand the WebSocket Protocol.  A
+     * SHA-1 hash (160 bits) [FIPS.180-3], base64-encoded (see Section 4 of
+     * [RFC4648]), of this concatenation is then returned in the server's
+     * handshake.
+     *
+     * @param secWebSocketKey The value of the "Sec-WebSocket-Key" header field in the handshake request.
+     * @return the value of the "Sec-WebSocket-Accept" header field in the handshake response.
+     */
     private static String encodeSha1AndBase64(String secWebSocketKey) {
-        String string = secWebSocketKey + MAGIC_NUMBER;
+        String string = secWebSocketKey + GUID;
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-1");
             byte[] sha1Result = digest.digest(string.getBytes());
@@ -30,13 +57,14 @@ public class WebSocketServer {
     public static void main(String[] args) {
         try (ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
             serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.bind(new InetSocketAddress(3000));
+            serverSocketChannel.bind(new InetSocketAddress(PORT));
 
             Selector selector = Selector.open();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
             while (true) {
                 selector.select(1);
-                for (Iterator<SelectionKey> it = selector.selectedKeys().iterator(); it.hasNext(); ) {
+                Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+                while (it.hasNext()) {
                     SelectionKey key = it.next();
                     if (key.isAcceptable()) {
                         SocketChannel channel = serverSocketChannel.accept();
@@ -47,21 +75,34 @@ public class WebSocketServer {
                     }
                     if (key.isReadable()) {
                         SocketChannel socketChannel = (SocketChannel) key.channel();
-                        ByteBuffer buffer = ByteBuffer.allocate(2048);
+                        ByteBuffer buffer = ByteBuffer.allocate(256);
                         socketChannel.read(buffer);
                         buffer.flip();
 
-                        // String byteString = toByteString(buffer);
                         String string = new String(buffer.array());
                         if (string.startsWith("GET / HTTP/1.1")) {
-                            int swkIndex = string.indexOf("Sec-WebSocket-Key:");
-                            int endIndex = string.indexOf("\r\n", swkIndex);
-                            String swk = string.substring(swkIndex + 19, endIndex);
+                            Pattern secWSKeyPattern = Pattern.compile("Sec-WebSocket-Key:\\s*(.*?)\r\n");
+                            Matcher matcher = secWSKeyPattern.matcher(string);
 
-                            String s = encodeSha1AndBase64(swk);
+                            // If the Sec-WebSocket-Key header is not present, close the connection
+                            if (!matcher.find()) {
+                                closeSocketChannel(socketChannel, key);
+                                continue;
+                            }
 
-                            String response = "HTTP/1.1 101 Switching Protocols\r\n" + "Upgrade: websocket\r\n" + "Connection: Upgrade\r\n" + "Sec-WebSocket-Accept: " + s + "\r\n\r\n";
-                            socketChannel.write(ByteBuffer.wrap(response.getBytes()));
+                            // Generate the Sec-WebSocket-Accept header value
+                            String secWebSocketKey = matcher.group(1);
+                            String secWSAcceptHeader = encodeSha1AndBase64(secWebSocketKey);
+
+                            // Create the handshake response
+                            StringJoiner responseBuilder = new StringJoiner("\r\n");
+                            responseBuilder.add("HTTP/1.1 101 Switching Protocols");
+                            responseBuilder.add("Upgrade: websocket");
+                            responseBuilder.add("Connection: Upgrade");
+                            responseBuilder.add("Sec-WebSocket-Accept: " + secWSAcceptHeader);
+                            responseBuilder.add("\r\n");
+                            // Send the handshake response
+                            socketChannel.write(ByteBuffer.wrap(responseBuilder.toString().getBytes()));
                         } else {
                             if (buffer.limit() <= 0) continue;
 
@@ -134,23 +175,25 @@ public class WebSocketServer {
                              * zero, in which case the payload length is the length of the
                              * "Application data".
                              */
-                            long length = secondByte & 0b01111111;
-                            switch ((int) length) {
+                            int length = secondByte & 0b01111111;
+                            switch (length) {
+                                case 127 -> length = (int) buffer.getLong();
                                 case 126 -> length = buffer.getShort();
-                                case 127 -> length = buffer.getLong();
                             }
 
-                            byte[] dataBytes = new byte[(int) length];
+                            // If the mask bit is not set, close the connection
+                            if (mask != 1) {
+                                closeSocketChannel(socketChannel, key);
+                                continue;
+                            }
 
-                            byte[] maskValue = null;
-                            if (mask == 1) {
-                                // Unmask the data
-                                maskValue = new byte[4];
-                                buffer.get(maskValue);
-                                for (int i = 0; i < length; i++) {
-                                    byte data = buffer.get();
-                                    dataBytes[i] = (byte) (data ^ maskValue[i % 4]);
-                                }
+                            byte[] dataBytes = new byte[length];
+                            byte[] maskValue = new byte[4];
+                            buffer.get(maskValue);
+                            // Apply the mask to the data
+                            for (int i = 0; i < length; ++i) {
+                                byte data = buffer.get();
+                                dataBytes[i] = (byte) (data ^ maskValue[i % 4]);
                             }
                             System.out.println(new String(dataBytes));
                         }
@@ -162,6 +205,12 @@ public class WebSocketServer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void closeSocketChannel(SocketChannel socketChannel, SelectionKey key) throws IOException {
+        socketChannel.socket().close();
+        socketChannel.close();
+        key.cancel();
     }
 }
 
